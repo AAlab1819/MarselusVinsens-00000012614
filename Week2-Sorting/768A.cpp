1cd0fe2d148ab224405b69772ce3f0ef21f060a0
mvinsens <mvinsens@users.noreply.github.com>
2018-09-09 19:37:22 +0700

    Update 768A.cpp

--- Week2-Sorting/768A.cpp
+++ Week2-Sorting/768A.cpp
@@ -1,46 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void heapify(int arr[], int n, int i)
+void cocktailshaker(int arr[], int length)
 {
-    int largest = i; // Initialize largest as root
-    int l = 2*i + 1; // left = 2*i + 1
-    int r = 2*i + 2; // right = 2*i + 2
- 
-    // If left child is larger than root
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
- 
-    // If right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
- 
-    // If largest is not root
-    if (largest != i)
-    {
-        swap(arr[i], arr[largest]);
- 
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
-    }
-}
- 
-// main function to do heap sort
-void heapSort(int arr[], int n)
-{
-    // Build heap (rearrange array)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
- 
-    // One by one extract an element from heap
-    for (int i=n-1; i>=0; i--)
-    {
-        // Move current root to end
-        swap(arr[0], arr[i]);
- 
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
+	bool swapped = false;
+	do
+	{
+		swapped = false;
+		for(int i = 0; i < length-1; i++)
+		{
+			if(arr[i] > arr[i+1])
+			{
+				swap(arr[i],arr[i+1]);
+				swapped = true;
+			}
+		}
+		if(!swapped) break;
+		
+		swapped = false;
+		for(int j = length -2; j>=0; j--)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				swap(arr[j],arr[j+1]);
+				swapped = true;
+			}
+		}
+	}
+	while(!swapped);
 }
 
 int main()
@@ -56,7 +43,7 @@ int main()
     	cin >> steward[i];
 	}
 	
-	heapSort(steward, numstew);
+	cocktailshaker(steward, numstew);
 		
 	int weak = steward[0];
 	int strong = steward[numstew-1];
